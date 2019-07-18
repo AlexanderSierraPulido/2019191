1d8553cea361be98acf35a78640318c6021a224d
Default user <a.sierra12@uniandes.edu.co>
2019-07-18 20:46:10 +0000

    wedwse

--- Tarea2/Plots_hw2.py
+++ Tarea2/Plots_hw2.py
@@ -2,6 +2,12 @@ import numpy as np
 import matplotlib.pylab as plt
 
 Euler = np.genfromtxt("Euler.txt",delimiter = ",")
+Leap = np.genfromtxt("Leap_frog.txt",delimiter = ",")
+
 plt.figure()
 plt.plot(Euler[:,0],Euler[:,1])
 plt.savefig("prueba")
+
+plt.figure()
+plt.plot(Leap[:,0],Leap[:,1])
+plt.savefig("prueba2")
