279c72699f4bc4ea2091fc8ac9e8cd05b9d19736
Default user <a.sierra12@uniandes.edu.co>
2019-07-19 05:51:44 +0000

    Trasnochar sirve

--- Tarea2/ODEs.cpp
+++ Tarea2/ODEs.cpp
@@ -10,27 +10,34 @@ void Runge_kutta(float dt,string file);
 float dv_x(float x,float y);
 float dv_y(float x,float y);
 
+float G = 1.982*pow(10,-29);
+float M = 1.989*pow(10,30);
+float m = 5.972*pow(10,24);
+
 int main()
     {
     Euler(0.005,"Euler_0.005.txt");
-    Euler(0.001,"Euler_0.001.txt");
-    Leap_frog(0.005,"Leap_frog.txt");
-    
+    Euler(0.0025,"Euler_0.0025.txt");
+    Euler(0.0005,"Euler_0.0005.txt");
+    Leap_frog(0.005,"Leap_0.005.txt");
+    Leap_frog(0.0025,"Leap_0.0025.txt");
+    Leap_frog(0.0005,"Leap_0.0005.txt");
+    Runge_kutta(0.005,"RK_0.005.txt");
+    Runge_kutta(0.0025,"RK_0.0025.txt");
+    Runge_kutta(0.0005,"RK_0.0005.txt");
+
     return 0;
     }
 
 float dv_x(float x,float y)
     {
-    float G = 1.982*pow(10,-29);
-    float M = 1.989*pow(10,30);
     float r = sqrt(pow(x,2)+pow(y,2));
     return -G*M*x/pow(r,3);
     }
 
 float dv_y(float x,float y)
     {
-    float G = 1.982*pow(10,-29);
-    float M = 1.989*pow(10,30);
+
     float r = sqrt(pow(x,2)+pow(y,2));
     return -G*M*y/pow(r,3);
     }
@@ -41,13 +48,16 @@ void Euler(float dt,string file)
     float vx_pas = -6.35, vy_pas = 0.606;
     float t =0, t_fin = 20;
     float x_fut,y_fut,vx_fut,vy_fut;
+    float L,E;
     
     ofstream outfile;
     outfile.open(file);
     
     while(t<t_fin)
         {
-        outfile<<x_pas<<","<<y_pas<<","<<vx_pas<<","<<vy_pas<<","<<t<<endl;
+        L = (pow(vx_pas,2)+pow(vy_pas,2))*m*sqrt(pow(x_pas,2)+pow(y_pas,2));
+        E = 0.5*m*(pow(vx_pas,2)+pow(vy_pas,2)) - G*M*m/sqrt(pow(x_pas,2)+pow(y_pas,2));
+        outfile<<x_pas<<","<<y_pas<<","<<vx_pas<<","<<vy_pas<<","<<L<<","<<E<<","<<t<<endl;
         x_fut = x_pas + dt*vx_pas;
         y_fut = y_pas + dt*vy_pas;
         vx_fut = vx_pas + dt*dv_x(x_pas,y_pas);
@@ -67,13 +77,16 @@ void Leap_frog(float dt,string file)
     float vx_pas = -6.35, vy_pas = 0.606;
     float t =0, t_fin = 20;
     float x_fut,y_fut,vx_fut,vy_fut,x_pre,y_pre,vx_pre,vy_pre;
+    float L,E;
 
     ofstream outfile;
     outfile.open(file);
     
     while(t<t_fin)
         {
-        outfile<<x_pas<<","<<y_pas<<","<<vx_pas<<","<<vy_pas<<","<<t<<endl;
+        L = (pow(vx_pas,2)+pow(vy_pas,2))*m*sqrt(pow(x_pas,2)+pow(y_pas,2));
+        E = 0.5*m*(pow(vx_pas,2)+pow(vy_pas,2)) - G*M*m/sqrt(pow(x_pas,2)+pow(y_pas,2));
+        outfile<<x_pas<<","<<y_pas<<","<<vx_pas<<","<<vy_pas<<","<<L<<","<<E<<","<<t<<endl;
         x_pre = vx_pas*2*dt + x_pas;
         y_pre = vy_pas*2*dt + y_pas;
         x_fut = 2*dt*vx_pas + x_pas;
@@ -99,13 +112,16 @@ void Runge_kutta(float dt,string file)
     float k1_y,k2_y,k3_y,k4_y;
     float k1_vx,k2_vx,k3_vx,k4_vx;
     float k1_vy,k2_vy,k3_vy,k4_vy;
+    float L,E;
     
     ofstream outfile;
     outfile.open(file);
     
     while(t<t_fin)
         {
-        outfile<<x_pas<<","<<y_pas<<","<<vx_pas<<","<<vy_pas<<","<<t<<endl;
+        L = (pow(vx_pas,2)+pow(vy_pas,2))*m*sqrt(pow(x_pas,2)+pow(y_pas,2));
+        E = 0.5*m*(pow(vx_pas,2)+pow(vy_pas,2)) - G*M*m/sqrt(pow(x_pas,2)+pow(y_pas,2));
+        outfile<<x_pas<<","<<y_pas<<","<<vx_pas<<","<<vy_pas<<","<<L<<","<<E<<","<<t<<endl;
         
         k1_x = vx_pas;
         k1_y = vy_pas;
