3d8649118cc532ffa4e2db604c137bdb7157b402
Default user <a.sierra12@uniandes.edu.co>
2019-07-19 14:12:26 +0000

    jaja

--- Tarea2/Fourier.py
+++ Tarea2/Fourier.py
@@ -10,39 +10,39 @@ FFT_sonrie = np.fft.fft2(sonrie)
 Seria_freq = np.fft.fftshift(FFT_seria)
 Sonrie_freq = np.fft.fftshift(FFT_sonrie)
 
-plt.figure()
+plt.figure()                                    ###### Plot transformada
 plt.subplot(1,2,1)
-plt.imshow(abs(FFT_seria),plt.cm.gray, norm = LogNorm() )
+plt.imshow(abs(FFT_seria),norm = LogNorm() )
 plt.colorbar()
 plt.title("Seria transformada")
 
 plt.subplot(1,2,2)
-plt.imshow(abs(FFT_sonrie),plt.cm.gray, norm = LogNorm() )
+plt.imshow(abs(FFT_sonrie),norm = LogNorm() )
 plt.colorbar()
 plt.title("Sonrie transformada")
-plt.savefig("FFtIm")
+plt.savefig("FFtIm.pdf")
 
+ 
+for i in range(254):                      
+    for j in range(170):
+        if(abs(Seria_freq[i,j]) <= 110 ):
+             FFT_seria[i,j] = 0
 
 for i in range(254):
     for j in range(170):
-        if(abs(FFT_seria[i,j]) <= 110 ):
-            Seria_freq[i,j] = 0
-
-for i in range(254):
-    for j in range(170):
-        if(abs(FFT_sonrie[i,j]) >= 45 ):
-            Sonrie_freq[i,j] = 0          
+        if(abs(Sonrie_freq[i,j]) >= 42 ):
+             FFT_sonrie[i,j]= 0          
 
 Filtro_seria = np.fft.ifft2(FFT_seria)
 Filtro_sonrie = np.fft.ifft2(FFT_sonrie)
 
 plt.figure()
 plt.subplot(2,2,1)
-plt.imshow(abs(Filtro_seria),norm= LogNorm(),cmap='Greys')
+plt.imshow(abs(Filtro_seria),norm= LogNorm())
 plt.title("Seria Filtrada")
 
 plt.subplot(2,2,2)
-plt.imshow(abs(Filtro_sonrie),norm= LogNorm(),cmap='Greys')
+plt.imshow(abs(Filtro_sonrie),norm= LogNorm())
 plt.title("Sonrie Filtrada")
 
 Filtro_seria_shift = np.fft.fftshift(Filtro_seria)
@@ -50,22 +50,17 @@ Filtro_sonrie_shift = np.fft.fftshift(Filtro_sonrie)
 
 plt.subplot(2,2,3)
 plt.imshow(abs(Filtro_seria_shift),norm= LogNorm())
-plt.title("Espacio de amplitudes filtro seria")
+plt.title("Espacio_amplitudes_filtro_seria")
 
 plt.subplot(2,2,4)
 plt.imshow(abs(Filtro_sonrie_shift),norm= LogNorm())
-plt.title("Espacio de amplitudes filtro sonrie")
+plt.title("Amplitudes_filtro_sonrie")
 plt.savefig("ImProceso")
 
-#Creando la imagen hibrida a partir de las imagenes filtradas anteriormente
 
 Total = 0.75*Sonrie_freq + 2.5*Seria_freq
 
-#Recuperando la imagen hibrida usando la transformada de Fourier inversa
-
 Imag_hib = np.fft.ifft2(Total)
-
-#Graficando la imagen hibrida 
 
 plt.figure()
 plt.imshow(-Imag_hib.real,cmap='Greys')
