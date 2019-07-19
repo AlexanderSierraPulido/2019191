79ac1dca18ba1b92e9223288f92435c1a01fdd2c
Default user <a.sierra12@uniandes.edu.co>
2019-07-19 14:15:44 +0000

    makefile

--- Tarea2/hw2.mk
+++ Tarea2/hw2.mk
@@ -1,12 +1,10 @@
-Resultados.pdf: latex
-    pdf latex
-
-Latex : Fourier.py Plots_hw2.py
+Resultados_hw2.pdf : Resultados_hw2.tex 
+    pdflatex Resultados_hw2.tex
+    
+Resultados_hw2.tex : Fourier.py Plots_hw2.py
     python Fourier.py 
     python Plots_hw2.py
     
-Plots_hw2.py : datos.txt
-
-datos.txt : ODEs.cpp
+Plots_hw2.py : ODEs.cpp
     g++ ODEs.cpp
     ./a.out
